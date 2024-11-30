0d21c3929b825293aa8da7c282d370a17b129d2a
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-12-01 00:02:30 +0530

    user auth: register, login & auth middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/auth-router.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-    });
+  res.send("Hello World!");
+});
+
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ backend/src/controllers/auth-controller.js
@@ -0,0 +1,57 @@
+import User from "../models/user-model.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/jwt.js";
+
+export async function register(req, res) {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    // Create password hash
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({ name, email, password: passwordHash });
+    const token = generateToken(user);
+
+    return res.status(201).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export async function login(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if password is valid by comparing the hash with the input password
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = generateToken(user);
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export async function me(req, res) {
+  return res.status(200).json(req.user);
+}
