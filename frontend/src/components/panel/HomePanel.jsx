01906e23e389d89a4ed1f8610d09c7ae24f286c6
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-12-04 13:15:10 +0530

    renamed logs folder to logs-components

--- frontend/src/components/panel/HomePanel.jsx
+++ frontend/src/components/panel/HomePanel.jsx
@@ -1,11 +1,9 @@
-import LogList from "./logs/LogList";
+import LogList from "./logs-components/LogList";
 import { UserContext } from "../../context/UserContext";
 import { useContext } from "react";
 
 export default function HomePanel() {
   const { hasRole, hasPermission } = useContext(UserContext);
-
-  console.log(hasRole("admin"));
 
   return (
     <div className="flex flex-col text-4xl h-[500px] gap-10">
